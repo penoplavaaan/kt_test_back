fce3d6c64c77854a2cc825de59700270d2b83f46
penoplavaan <penoplavaaan@gmail.com>
2022-12-20 02:28:16 +0500

    .

---
+++ src/Entity/Category.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="categories")
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $name;
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
