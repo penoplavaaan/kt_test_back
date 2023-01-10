33fdcea5926dd17f6e904337e4492e4460b1a6ab
penoplavaan <alex@penoplavaan.space>
2023-01-10 20:51:35 +0500

    feat: add final tests for categoryController

--- tests/Controller/CategoryControllerTest.php
+++ tests/Controller/CategoryControllerTest.php
@@ -3,13 +3,31 @@
 namespace App\Tests\Controller;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use JetBrains\PhpStorm\NoReturn;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
+/**
+ * ВВОДНЫЕ:
+ * Создается 30 экземпляров сущности Product,
+ * посредством фикстуры в App\DataFixtures\ProductFixture.php,
+ * и 15 экземпляров сущности Category,
+ * посредством фикстуры в App\DataFixtures\CategoryFixture.php,
+ * по два продукта на каждую категорию
+ */
 class CategoryControllerTest extends ApiTestCase
 {
+    private const CATEGORY_COUNT = 15;
+
     /**
      * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
      */
+    #[NoReturn]
     public function testList(): void
     {
         $client = static::createClient();
@@ -20,9 +38,7 @@ class CategoryControllerTest extends ApiTestCase
             $client->getResponse()->getContent()
         );
 
-        dd($response);
-
         $this->assertResponseIsSuccessful();
-//        $this->assertJsonContains(['@id' => '/']);
+        $this->assertCount(self::CATEGORY_COUNT, $response);
     }
 }
